30afdeddbbbc3d2015e8f9ff37aeeb1a5306a5f4
Sherfin Shamsudeen <sherfin94@gmail.com>
2021-06-26 14:03:14 +0530

    Update label DATE to DATE_TOKENS

--- process.py
+++ process.py
@@ -6,6 +6,7 @@ class TextProcessor:
   def __init__(self) -> None:
     spacy.prefer_gpu()
     self.nlp = spacy.load("en_core_web_sm")
+    self.entity_types = ['PERSON', 'NORP', 'FAC', 'ORG', 'GPE', 'LOC', 'PRODUCT', 'EVENT', 'WORK_OF_ART', 'LAW', 'LANGUAGE', 'DATE_TOKENS', 'TIME', 'PERCENT', 'MONEY', 'QUANTITY', 'ORDINAL', 'CARDINAL']
     self.elastic = Elastic()
 
   def tokens(self, doc):
@@ -15,35 +16,50 @@ class TextProcessor:
       if not lexeme.is_stop and not lexeme.is_punct: result.append(token.text)
     return result
   
+  def generate_entities(self, doc):
+    entities = {}
+    for entity in doc.ents:
+      if entity.label_ in self.entity_types:
+        entities[entity.label_] = entity.text
+    
+    for entity_type in self.entity_types:
+      if entity_type not in entities:
+        entities[entity_type] = []
+
+    return entities
+
+
   def process(self, text):
     doc = self.nlp(text['content'])
-    people = list(set([entity.text for entity in doc.ents if entity.label_ == 'PERSON']))
-    gpe = list(set([entity.text for entity in doc.ents if entity.label_ == 'GPE']))
     tokens = self.tokens(doc)
     id = text['id']
     source_id = text['source_id']
     content = text['content']
     author = text['author']
-    date = text.get('date')
+    date = text['date']
     created_at = text['created_at']
     updated_at = text['updated_at']
     deleted_at = text['deleted_at']
 
+    entities = self.generate_entities(doc)
+    print(entities)
 
-    self.save(id, content, author, date, people, gpe, tokens, source_id, created_at, updated_at, deleted_at)
+    self.save(id, content, author, date, tokens, source_id, created_at, updated_at, deleted_at, entities)
 
-  def save(self, id, content, author, date, people, gpe, tokens, source_id, created_at, updated_at, deleted_at):
+  def save(self, id, content, author, date, tokens, source_id, created_at, updated_at, deleted_at, entities):
     payload = {
       'content': content,
       'author': author,
-      'people': people,
-      'gpe': gpe,
       'tokens': tokens,
       'source_id': source_id,
+      'date': date,
       'created_at': created_at,
       'updated_at': updated_at,
       'deleted_at': deleted_at
     }
-    if date: payload['date'] = date
+
+    payload.update(entities)
+
+    print(payload)
 
     self.elastic.save(payload, id)
